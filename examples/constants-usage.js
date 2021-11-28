e45449c567f6f0657d2836ca101959cdc723d878
Foile <Foile@users.noreply.github.com>
2021-11-28 17:10:54 +0300

    Reworking to CommonJS module

--- examples/constants-usage.js
+++ examples/constants-usage.js
@@ -1,14 +1,10 @@
 /* eslint-disable no-console */
-import { CryptoPay, Assets, PaidButtonNames } from '@foile/crypto-pay-api';
+const { CryptoPay, Assets, PaidButtonNames } = require('@foile/crypto-pay-api');
 
 const token = '42:YOURTOKEN';
 
 (async function run() {
   const cryptoPay = new CryptoPay(token, { hostname: 'testnet-pay.crypt.bot' });
-
-  // A simple method for testing your app's authentication token
-  const app = await cryptoPay.getMe();
-  console.log(app);
 
   // Create a new invoice with additional data
   const invoice = await cryptoPay.createInvoice(Assets.BTC, 1, {
